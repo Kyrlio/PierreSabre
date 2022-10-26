99d0205889ae6cae8562fdc039fc477365a95fb8
Kyrlio <cedricroussel2002@gmail.com>
2022-10-26 14:33:39 +0200

    création de la classe Humain

---
+++ src/histoire/HistoireTP4.java
@@ -0,0 +1,18 @@
+package histoire;
+
+import personnages.Humain;
+
+public class HistoireTP4 {
+	
+	public static void main(String[] args) {
+		Humain prof = new Humain("Prof", "kombucha", 54);
+		prof.direBonjour();
+		prof.parler("J'ai " + prof.getQteArgent() + " sous en poche. Je vais pouvoir m'offrir une boisson à 12 sous");
+		prof.boire();
+		prof.perdreArgent(12);
+		prof.parler("J'ai " + prof.getQteArgent() + " sous en poche. Je vais pouvoir m'offrir un jeu à 2 sous");
+		prof.perdreArgent(2);
+		prof.parler("Je n'ai plus que " + prof.getQteArgent() + " sous en poche. Je ne peux même pas m'offrir un kimono à 50 sous.");
+ }
+
+}
---
+++ src/personnages/Humain.java
@@ -0,0 +1,50 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String boissonFav;
+	private int qteArgent;
+	
+	public Humain(String nom, String boissonFav, int qteArgent) {
+		this.nom = nom;
+		this.boissonFav = boissonFav;
+		this.qteArgent = qteArgent;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public String getBoissonFav() {
+		return boissonFav;
+	}
+	public int getQteArgent() {
+		return qteArgent;
+	}
+	
+	public void parler(String texte) {
+		System.out.println("[" + nom +"] - " + texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle " + nom + " et j'aime boire du " + boissonFav);
+	}
+	
+	public void boire() {
+		parler("Mmmm, un bon verre de " + boissonFav + " ! GLOUPS !");
+	}
+	
+	
+	public void acheter(String bien, int prix) {
+		if (qteArgent >= prix) {
+			perdreArgent(prix);
+		} else {
+			parler("Je n'ai pas assez d'argent pour acheter un " + bien);
+		}
+	}
+	
+	public void perdreArgent(int perte) {
+		this.qteArgent -= perte;
+	}
+	
+
+}
